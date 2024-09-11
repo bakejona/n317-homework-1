94ae9f8cf423764eaf012002136d29401af91b52
bakejona <jbakerbba2@gmail.com>
2024-09-11 11:15:04 -0400

    Added about content

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,17 +1,19 @@
 "use client";
 import homeStyles from "@/app/page.module.css";
+import aboutStyles from "@/app/about/about.module.css"
 
 export default function About() {
     return (
         <div>
           <nav style={{ display: 'flex', justifyContent: 'space-evenly', backgroundColor: '#f0f0f0', padding: '1rem' }}>
-            <a href="/home" style={{ textDecoration: 'none', fontWeight: 'bold', color: 'black' }}>We Sell Nothing.</a>
+            <a href="#" style={{ textDecoration: 'none', fontWeight: 'bold', color: 'black' }}>We Sell Nothing.</a>
             <a href="/about" style={{ textDecoration: 'none', color: '#333' }}>About</a>
             <a href="#" style={{ textDecoration: 'none', color: '#333' }}>Link 2</a>
             <a href="#" style={{ textDecoration: 'none', color: '#333' }}>Link 3</a>
           </nav>
           <main>
             <h1>About</h1>
+            <p>We sell nothing</p>
           </main>
         </div>
         );
